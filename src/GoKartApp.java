1ce978af493c8a516435cb82a7cdb92585c0608a
Eric Price <wunderkammer123@gmail.com>
2016-06-19 14:08:21 -0500

    initial commit. Some Java basics review

---
+++ src/GoKartApp.java
@@ -0,0 +1,10 @@
+
+public class GoKartApp {
+	public static void main(String[] args) {
+		GoKart kart = new GoKart("yellow");
+		if (kart.isBatteryEmpty()) {
+			System.out.println("The battery is empty");
+		}
+		kart.drive(2);
+	}
+}
